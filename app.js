569269298ee324c6bcff5030e06267d515748201
tomy.dev <xxx.d1231@gmail.com>
2024-12-19 18:06:35 +0900

    新規ページ追加のルーティングを追加

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
+const campground = require("./models/campground");
 
 // mongooseへの接続
 mongoose
@@ -20,20 +21,36 @@ mongoose
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 // ホームディレクトリのルーティング
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-// makecampgroundルーティング
-app.get("/makecampground", async (req, res) => {
-  const camp = new Campground({
-    title: "私の庭",
-    description: "気軽にキャンプ",
-  });
-  await camp.save();
-  res.send(camp);
+// campgroundルーティング(indexページ)
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
+
+// 新規追加ページのルーティング(ルーティングの場所に注意！)
+app.get("/campgrounds/new", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/new");
+});
+
+// 新規登録フォームの送信先のルーティング
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground.id}`);
+});
+
+// 詳細ページへのルーティング
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
 });
 
 // サーバー立ち上げ
