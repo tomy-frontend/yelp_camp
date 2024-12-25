703fe87450056b874810d2aabecc2b6cbd386289
tomy.dev <xxx.d1231@gmail.com>
2024-12-25 11:36:04 +0900

    reviewスキーマのバリデーションとフロントへの表示処理を追加

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const morgan = require("morgan");
 const ejsMate = require("ejs-mate");
 const catchAsync = require("./utils/catchAsync"); // エラーハンドリング自作クラスの呼び出し
 const ExpressError = require("./utils/ExpressError");
-const { campgroundSchema } = require("./schemas");
+const { campgroundSchema, reviewSchema } = require("./schemas");
 
 // mongooseへの接続
 mongoose
@@ -42,6 +42,17 @@ app.use("/campgrounds/:id", (req, res, next) => {
 // 新規追加と更新の際のバリデーションチェック自作ミドルウェア
 const validateCampground = (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((detail) => detail.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next(); // 問題なければ次の処理に進む(必須！これがないと処理止まる)
+  }
+};
+
+// reviewのバリデーションチェック自作ミドルウェア
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
   if (error) {
     const msg = error.details.map((detail) => detail.message).join(",");
     throw new ExpressError(msg, 400);
@@ -90,7 +101,9 @@ app.post(
 app.get(
   "/campgrounds/:id",
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate(
+      "reviews"
+    );
     res.render("campgrounds/show", { campground });
   })
 );
@@ -130,8 +143,12 @@ app.delete(
   })
 );
 
+// reviewの投稿追加のルーティング
+// + Joiでサーバーサイドのバリデーションチェック
+// + catchAsync関数 → 非同期処理でエラーがあったらエラーハンドリングミドルウェアに渡すミドルウェア
 app.post(
   "/campgrounds/:id/reviews",
+  validateReview,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
--- schemas.js
+++ schemas.js
@@ -1,5 +1,6 @@
 const Joi = require("joi");
 
+// campground用のサーバーサイドバリデーション
 module.exports.campgroundSchema = Joi.object({
   campground: Joi.object({
     title: Joi.string().required(),
@@ -7,5 +8,13 @@ module.exports.campgroundSchema = Joi.object({
     image: Joi.string().required(),
     location: Joi.string().required(),
     description: Joi.string().required(),
+  }).required(),
+});
+
+// review用のサーバーサイドバリデーション
+module.exports.reviewSchema = Joi.object({
+  review: Joi.object({
+    rating: Joi.number().required().min(1).max(5),
+    body: Joi.string().required(),
   }).required(),
 });
