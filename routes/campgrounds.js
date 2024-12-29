98d8359909997516bd1a6ecd36775332e619b5a8
tomy.dev <xxx.d1231@gmail.com>
2024-12-29 22:27:58 +0900

    routerをパスごとにまとめるリファクタリング

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,39 +4,33 @@ const campgrounds = require("../controllers/campgrounds");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 const router = express.Router();
 
-router.get("/", catchAsync(campgrounds.index));
+router
+  .route("/")
+  .get(catchAsync(campgrounds.index))
+  .post(
+    isLoggedIn,
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
-router.get("/:id", catchAsync(campgrounds.showCampground));
-
-router.post(
-  "/",
-  isLoggedIn,
-  validateCampground,
-  catchAsync(campgrounds.createCampground)
-);
+router
+  .route("/:id")
+  .get(catchAsync(campgrounds.showCampground))
+  .put(
+    isLoggedIn,
+    isAuthor,
+    validateCampground,
+    catchAsync(campgrounds.updateCampground)
+  )
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get(
   "/:id/edit",
   isLoggedIn,
   isAuthor,
   catchAsync(campgrounds.renderEditForm)
-);
-
-router.put(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  validateCampground,
-  catchAsync(campgrounds.updateCampground)
-);
-
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  catchAsync(campgrounds.deleteCampground)
 );
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,36 +3,29 @@ const router = express.Router(); // mergeParamsで明示的に宣言する必要
 const passport = require("passport");
 const users = require("../controllers/users");
 
-// GET /register 登録ページのルート
-router.get("/register", users.renderRegister);
+router.route("/register").get(users.renderRegister).post(users.register);
 
-// POST /register 登録フォームのリクエスト先
-router.post("/register", users.register);
-
-// loginルート
-router.get("/login", users.renderLogin);
-
-// POST /login ログインフォームのリクエスト先
-// passportのミドルウェアを差し込んで、optionを設定するだけで認証プロセスを実行してくれる
-router.post(
-  "/login",
-  (req, res, next) => {
-    const returnTo = req.session.returnTo;
-    req.tempReturnTo = returnTo; // reqオブジェクトに一時保存
-    next();
-  },
-  passport.authenticate("local", {
-    failureFlash: true,
-    failureRedirect: "/login",
-  }),
-  (req, res) => {
-    req.flash("success", "おかえりなさい！");
-    // 一時保存した値を使用
-    const redirectUrl = req.tempReturnTo || "/campgrounds";
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-  }
-);
+router
+  .route("/login")
+  .get(users.renderLogin)
+  .post(
+    (req, res, next) => {
+      const returnTo = req.session.returnTo;
+      req.tempReturnTo = returnTo; // reqオブジェクトに一時保存
+      next();
+    },
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+    }),
+    (req, res) => {
+      req.flash("success", "おかえりなさい！");
+      // 一時保存した値を使用
+      const redirectUrl = req.tempReturnTo || "/campgrounds";
+      delete req.session.returnTo;
+      res.redirect(redirectUrl);
+    }
+  );
 
 // ログアウトルート
 router.get("/logout", users.logout);
