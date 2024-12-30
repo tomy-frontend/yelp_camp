c534c3088d3952a4b7bf2cf15e86a18edf8efab4
tomy.dev <xxx.d1231@gmail.com>
2024-12-30 23:46:03 +0900

    クラスターマップのポップアップメッセージ追加

--- models/campground.js
+++ models/campground.js
@@ -11,34 +11,44 @@ imageSchema.virtual("thumbnail").get(function () {
   return this.url.replace("/upload", "/upload/w_200");
 });
 
+const opts = { toJSON: { virtuals: true } };
+
 // キャンプ場のスキーマ
-const campgroundSchema = new Schema({
-  title: String,
-  images: [imageSchema],
-  geometry: {
-    type: {
-      type: String,
-      enum: ["Point"],
-      required: true,
+const campgroundSchema = new Schema(
+  {
+    title: String,
+    images: [imageSchema],
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
-  },
-  price: Number,
-  description: String,
-  location: String,
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  reviews: [
-    {
+    price: Number,
+    description: String,
+    location: String,
+    author: {
       type: Schema.Types.ObjectId,
-      ref: "Review",
+      ref: "User",
     },
-  ],
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  opts
+);
+
+campgroundSchema.virtual("properties.popupMarkup").get(function () {
+  return `<strong><a href="/campgrounds/${this._id}" >${this.title}</a></strong>
+  <p>${this.description.substring(0, 20)}...</p>`;
 });
 
 // キャンプ場が削除されると、紐づいているreviewも削除する関数
