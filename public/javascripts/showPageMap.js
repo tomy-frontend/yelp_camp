1f80dd42e2915658c75c9f83107e878acad46652
tomy.dev <xxx.d1231@gmail.com>
2024-12-30 22:53:35 +0900

    mapのスタイル変更

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,8 +1,15 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map",
+  style: "mapbox://styles/mapbox/light-v10",
   center: campground.geometry.coordinates,
   zoom: 12,
 });
 
-new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(`<h4>${campground.title}</h4>
+        <p>${campground.location}</p>`)
+  )
+  .addTo(map);
